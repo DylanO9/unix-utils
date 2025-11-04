caeebecbcf5a5e97ccf6b0ec8b59bb62b9cf7f9d
DylanO9 <dylan.olivares11@gmail.com>
2025-11-04 11:13:57 +0000

    feat: add is_valid_file to check if the file path given in arguments for wc are valid or not

--- wc/main.c
+++ wc/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <sys/stat.h> /* Gives us the stat struct*/
+#include <fcntl.h>
 
 typedef struct {
     unsigned newline_count;
@@ -6,17 +8,27 @@ typedef struct {
     unsigned byte_count;
 } Counts;
 
-Counts *wc(char *);
-int is_valid_file();
+Counts *my_wc(char *);
+int is_valid_file(char *);
 
-int main() {
-       
+int main(int argc, char *argv[]) {
 } 
 
-Counts *wc(char *file) {
-
+Counts *my_wc(char *file) {
+    
 }
 
-int is_valid_file() {
-
+int is_valid_file(char *file) {
+    struct stat stat_buf;
+    
+    if (stat(file, &stat_buf) == -1) {
+        fprintf(stderr, "Error: File could not be found %s\n", file);
+        return 0;
+    }
+    
+    if ((stat_buf.st_mode & S_IFMT) != S_IFREG) {
+        fprintf(stderr, "Error: Path provided is not a file %s\n", file);
+    }
+    
+    return 1;
 }
