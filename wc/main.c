f727cf76e7b2156a6e33f10a43adfe8b27dbf85b
DylanO9 <dylan.olivares11@gmail.com>
2025-11-04 10:51:43 +0000

    feat: setup wc file to have necessary functions and structure for implementation

---
+++ wc/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+typedef struct {
+    unsigned newline_count;
+    unsigned word_count;
+    unsigned byte_count;
+} Counts;
+
+Counts *wc(char *);
+int is_valid_file();
+
+int main() {
+       
+} 
+
+Counts *wc(char *file) {
+
+}
+
+int is_valid_file() {
+
+}
