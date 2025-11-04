11f5fb316c8cd89733979bc04cc71c5c4db8e44e
DylanO9 <dylan.olivares11@gmail.com>
2025-11-04 11:17:35 +0000

    feat: create function print_count to format the output in the way that wc does

--- wc/main.c
+++ wc/main.c
@@ -9,12 +9,27 @@ typedef struct {
 } Counts;
 
 Counts *my_wc(char *);
+void print_count(char *file, Counts *file_counts);
 int is_valid_file(char *);
 
 int main(int argc, char *argv[]) {
+    if (argc == 1) {
+        perror("There are not enough arguments\n");
+        return 1;
+    }  
+    char *file = *(argv + 1);
+    for (int i = 1; i < argc; file++, i++) {
+        Counts *current_counts;
+        if ((current_counts = my_wc(file)) == NULL) continue;
+        
+        print_count(file, current_counts);
+    }
+    
+    return 0;
 } 
 
 Counts *my_wc(char *file) {
+    if (!is_valid_file(file)) return NULL;
     
 }
 
@@ -31,4 +46,9 @@ int is_valid_file(char *file) {
     }
     
     return 1;
+}
+
+void print_count(char *file, Counts *file_counts) {
+    fprintf(stdout, "%d\t%d\t%d\t%s", file_counts->newline_count, file_counts->word_count,
+        file_counts->byte_count, file);
 }
