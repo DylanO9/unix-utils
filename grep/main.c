c41f4c5c0b7021f3a03daca3a94e05aa968a6301
DylanO9 <dylan.olivares11@gmail.com>
2025-11-05 07:59:01 +0000

    feat: add basic logic for determining if the file passed through my_grep arguments is a valid file or not

--- grep/main.c
+++ grep/main.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
+#include <sys/stat.h> /* Stat library for the struct stat */
+#include <fcntl.h> /* File control library that provides symbolic constants and function for file descriptors */
+#include <unistd.h> /* Unix standard library providing system calls */
 
 #define MAX_CHARS   1024
 
 int my_getline(char *);
 int is_valid_file(char*);
-char **my_grep(char *);
+int my_grep(char **, char *);
 
 int main (int argc, char *argv[]) {
         
 }
 
-char **my_grep(char *file) {
+int my_grep(char matched_lines, char *file) {
     // Initailize a buffer to read lines into    
 
     // Open the file using open()
@@ -19,7 +22,15 @@ char **my_grep(char *file) {
         // If no line exists, then exit
         // If a line exists -> look for the pattern in the line -> if the pattenr exists, print the line and line number
     // Close the file
-       
+    char *buf = malloc(MAX_CHARS); 
+    if (buf == NULL) {
+        perror("Error: Could not allocate memory\n");    
+        return 0;
+    }
+    
+    if (!is_valid_file(file)) {
+        fprintf(stderr, "Error: Could not access the file %s\n", file);
+    }
 }
 
 int my_getline(char *buf) {
@@ -29,5 +40,15 @@ int my_getline(char *buf) {
 }
 
 int is_valid_file(char *file) {
-
+    struct stat stbuf;
+    
+    if (stat(file, &stbuf) == -1) {
+        fprintf(stderr, "Error: The file could not be found %s\n", file);
+        return 1;
+    }
+    if ((stbuf.st_mode & S_IFMT) != S_IFREG) {
+        fprintf(stderr, "Error: The path provided does not lead to a file %s\n", file);    
+        return 1;
+    }
+    return 0;
 }
